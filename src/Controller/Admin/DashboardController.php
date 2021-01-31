7e638c9388988cce607c240e377f76f863839b6b
nashae <kesmoya@gmail.com>
2021-01-31 17:05:15 +0100

    admin,product, cart

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Shopsymf');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateur', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Categories', 'fas fa-box-open', Category::class);
+        yield MenuItem::linkToCrud('Produits', 'fas fa-cubes', Product::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
